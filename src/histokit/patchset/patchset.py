f02277976fcf905ef7b584070c64d82a38371f1e
David Morrison <dm236@st-andrews.ac.uk>
2026-04-13 11:50:50 +0100

    feature: add describe to patchset

--- src/histokit/patchset/patchset.py
+++ src/histokit/patchset/patchset.py
@@ -16,3 +16,22 @@ class PatchSet:
 
     def __repr__(self) -> str:
         return f"PatchSet(num_patches={len(self.frame)}, contexts={self.contexts}, manifest={self.manifest})"
+    
+    def describe(self) -> pd.DataFrame:
+        annotation_schema = None
+
+        # find the first annotation schema from the contexts to get the label map for counting annotation labels - assumes all contexts in the patchset have the same annotation schema if they have annotations at all
+        for ctx in self.contexts:
+            if ctx.sample.annotation_schema is not None:
+                annotation_schema = ctx.sample.annotation_schema
+                break
+
+        if annotation_schema is None:
+            return pd.DataFrame()
+
+        label_map = annotation_schema.label_map
+        counts = {
+            label: int((self.frame["annotation_label"] == value).sum())
+            for label, value in label_map.items()
+        }
+        return pd.DataFrame([counts])
